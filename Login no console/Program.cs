76530095417247265060322726274679e73115a1
Leonardo Almeida <131322465+AlmeidaLeoDev@users.noreply.github.com>
2023-06-05 16:47:22 -0300

    Update Program.cs

--- Login no console/Program.cs
+++ Login no console/Program.cs
@@ -1,45 +1,71 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace ProjetoLoginConsole
+namespace Cadastro_de_Usuarios 
 {
     internal class Program
     {
-        static void Main(string[] args)
+        //Método para deixar a senha invisível
+        private static string ReadPassword()
         {
-            while (true)
+            string senhaOculta = "";
+            ConsoleKeyInfo key;
+            do
             {
-                Console.WriteLine("Digite o nome do seu usuário:");
-                string usuario = Console.ReadLine();
-                Console.WriteLine("Digite a sua senha:");
-                string senha = "";
-                while (true)
+                key = Console.ReadKey(true);
+
+                if (key.Key != ConsoleKey.Backspace && key.Key != ConsoleKey.Enter)
                 {
-                    ConsoleKeyInfo tecla = Console.ReadKey(true);
-                    if (tecla.Key == ConsoleKey.Enter)
-                        break;
-                    else
-                    {
-                        senha += tecla.KeyChar;
-                    }
-                }
-                if (usuario == "Leonardo" && senha == "1234")
-                {
-                    Console.WriteLine("Usuário logado com sucesso");
-                    break;
+                    senhaOculta += key.KeyChar;
+                    Console.Write("*");
                 }
                 else
                 {
-                    Console.WriteLine("Usuário ou senha incorreto");
-                    Console.WriteLine("Pressione qualquer tecla ppara continuar");
-                    Console.ReadKey();
+                    if (key.Key == ConsoleKey.Backspace && senhaOculta.Length > 0)
+                    {
+                        senhaOculta = senhaOculta.Remove(senhaOculta.Length - 1);
+                        Console.Write("\b\b");
+                    }
+                }
+            } while (key.Key != ConsoleKey.Enter);
+
+            Console.WriteLine(); // Adicionando uma nova linha após a senha ser lida
+
+            return senhaOculta;
+        }
+
+        static void Main(string[] args)
+        {
+            string opcao = "";
+            do
+            {
+                Console.WriteLine("Digite C para se cadastrar ou S para sair");
+                opcao = Console.ReadKey(true).Key.ToString().ToLower();
+                if(opcao == "c")
+                {
+                    Console.WriteLine("Digite o seu nome de usuário: ");
+                    string usuario = Console.ReadLine();
+                    Console.WriteLine("Digite uma senha de até 10 digitos: ");
+                    string senha = ReadPassword();
+
+                    Console.WriteLine("O usuário e senha foram cadastrados com sucesso!");
+                    Console.ReadLine();
                     Console.Clear();
                 }
-            }
-            Console.WriteLine("Pressione qualquer tecla para sair");
+                else if(opcao == "s")
+                {
+                    Console.WriteLine("Encerrando o programa");
+                }
+                else
+                {
+                    Console.WriteLine("Opção desconhecida");
+                }
+            } while (opcao != "s");
+
+            Console.WriteLine("Pressione qualquer tecla para encerrar");
             Console.ReadKey();
         }
     }
